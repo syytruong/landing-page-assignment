a65511bbb5558348a74dd85240fb3f522dd2852c
Sy Truong <sy.truong@trackinno.com>
2023-04-29 15:02:48 +0300

    setup tailwindcss, simple first landing page to check if tailwind is working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="min-h-screen bg-gray-100">
+      {/* Main content */}
+      <div className="container mx-auto px-4">
+        <h1 className="text-4xl font-bold mt-8">Welcome to Our Website</h1>
+        <p className="text-lg mt-4">
+          Discover the amazing features we offer by signing up now!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          className="bg-blue-500 text-white px-6 py-2 rounded mt-6"
+          onClick={openModal}
         >
-          Learn React
-        </a>
-      </header>
+          Get Started
+        </button>
+      </div>
+
+      {/* Modal */}
+      {showModal && (
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-center justify-center min-h-screen">
+            <div className="bg-white p-8 rounded">
+              <h2 className="text-2xl mb-4">Enter Your Email</h2>
+              <form>
+                <input
+                  type="email"
+                  className="border border-gray-300 p-2 w-full mb-4"
+                  placeholder="Email address"
+                  required
+                />
+                <button
+                  className="bg-blue-500 text-white px-6 py-2 rounded w-full"
+                  type="submit"
+                >
+                  Continue
+                </button>
+              </form>
+              <button
+                className="text-gray-600 text-sm mt-4 underline"
+                onClick={closeModal}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+}
