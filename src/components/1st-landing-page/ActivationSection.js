7578d7ab003ff6d284ff72672b2fc47a865e5b1b
Sy Truong <sy.truong@trackinno.com>
2023-04-30 15:47:52 +0300

    add ActivationSection for mobile screen and laptop screen

---
+++ src/components/1st-landing-page/ActivationSection.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { FiSmartphone, FiDownload } from 'react-icons/fi';
+import { BsGlobeEuropeAfrica } from 'react-icons/bs';
+
+const ActivationSection = ({ openModal }) => {
+  return (
+    <div className="py-8">
+      {/* Mobile view */}
+      <div className="block lg:hidden text-center">
+        <div className="flex items-center justify-center">
+          <FiSmartphone className="text-xl" />
+          <h2 className="text-xl font-semibold ml-2">Mobile Activation</h2>
+        </div>
+        <button className="bg-green-700 text-white px-12 py-2 rounded mt-4 flex items-center justify-center mx-auto">
+          <FiDownload />
+          <span className="ml-2" onClick={openModal}>Continue</span>
+        </button>
+        <div className="mt-4 flex items-center justify-center">
+          <span className="bg-green-500 rounded-full h-2 w-2 mr-2"></span>
+          <span className="text-sm">High speed &amp; Unlimited</span>
+        </div>
+      </div>
+
+      {/* Laptop view */}
+      <div className="hidden lg:flex items-center justify-center">
+        <div className="text-start flex items-center w-48">
+          <BsGlobeEuropeAfrica className="text-5xl text-cyan-600 text-blue" />&nbsp;
+          <h2 className="text-xl text-cyan-600 font-semibold">Online<br /><span className="text-cyan-800">Activation</span></h2>
+        </div>
+        <div className="vertical-separator"></div>
+        <div className="flex flex-col items-center w-48">
+          <button className="bg-green-700 text-white px-12 py-2 rounded flex items-center">
+            <FiDownload />
+            <span className="ml-2" onClick={openModal}>Continue</span>
+          </button>
+          <div className="mt-2 flex items-center">
+            <span className="bg-green-500 rounded-full h-2 w-2 mr-2"></span>
+            <span className="text-xs">High speed &amp; Unlimited</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ActivationSection;
--- src/components/1st-landing-page/MainContent.js
+++ src/components/1st-landing-page/MainContent.js
@@ -1,20 +1,17 @@
 import React from 'react';
+import ActivationSection from './ActivationSection';
 
 const MainContent = ({ openModal }) => {
   return (
     <div className="container mx-auto px-4">
+      {/* Top disclaimation text */}
       <div className="text-center py-2 hidden lg:block flex items-center justify-center w-full">
         <p className="text-xs break-text">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent faucibus, ipsum lobortis hendrerit lacinia, nisi risus cursus turpis, vitae convallis ex elit id erat
         </p>
       </div>
-      {/* Add more sections here */}
-      <button
-        className="bg-blue-500 text-white px-6 py-2 rounded mt-4"
-        onClick={openModal}
-      >
-        Open Email Modal
-      </button>
+
+      <ActivationSection openModal={openModal} />
     </div>
   );
 };
